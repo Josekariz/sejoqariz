743d1c9d03538b5614fedecd3d50305058b1715c
Joseph <sejokarizz@gmail.com>
2024-05-20 16:40:23 +0300

    Added custom shades of black

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      //  Added custom shades of black
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+        },
       },
     },
   },
