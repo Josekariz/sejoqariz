52d69e7f66c170be8356fdac8a157e7e913b0ee7
Joseph <sejokarizz@gmail.com>
2024-05-20 17:14:35 +0300

    create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        Sejoqariz init 😁
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
