2b33efa9fe137f642c29e95ee53926b0909c2edd
Joseph <sejokarizz@gmail.com>
2024-05-20 17:27:21 +0300

    add spotlight

---
+++ components/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36 ">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vh]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-90  h-[80vh] w-[50vh]" fill="blue" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
