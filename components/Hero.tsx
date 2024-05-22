aa9e0682319129bee0ad98bfc91d3f50931acd26
Joseph <sejokarizz@gmail.com>
2024-05-22 19:04:06 +0300

    Add background grid lines with custom theme

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,8 @@ import { Spotlight } from "./ui/Spotlight";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36 ">
+      {/* Add spotlight animations */}
+
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -14,6 +16,17 @@ const Hero = () => {
           fill="purple"
         />
         <Spotlight className="top-28 left-90  h-[80vh] w-[50vh]" fill="blue" />
+      </div>
+
+      {/* Add bacground grid lines */}
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      <div className="flex justify-center">
+        <div></div>
       </div>
     </div>
   );
