cb556e95cf754dfee71af8a6073a4ba2afba1987
Joseph <sejokarizz@gmail.com>
2024-05-26 20:19:37 +0300

    Change file name to camel case

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,6 @@
 import { projects } from "@/data/idx";
 import React from "react";
-import { PinContainer } from "./ui/3d-pin";
+import { PinContainer } from "./ui/PinContainer";
 import { FaLocationArrow } from "react-icons/fa6";
 
 const RecentProjects = () => {
